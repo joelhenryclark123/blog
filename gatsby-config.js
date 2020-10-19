18eb134420b8a3ee81f267f80adaa1c3cb408dba
Joel Clark <joelhc@umich.edu>
2020-10-18 20:06:26 -0400

    changed site title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Joel's Blog`,
+    description: `I post here every Friday. Stop by to read about my latest work.`,
+    author: `Joel Clark`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
